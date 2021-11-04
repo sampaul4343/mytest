287e92c25447515a1524f28a846ca38d0b7d1929
sampaul4343 <93693807+sampaul4343@users.noreply.github.com>
2021-11-04 15:01:05 +0800

    Create test_client.py

---
+++ test_client.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+import datetime
+
+
+
+host = "localhost"
+port = 5000
+cln = socket.socket()
+cln.connect((host, port))
+print("connected"))
+
+
+
+try:
+  while True:
+    text = input("> ")
+    cln.sendall(text.encode("utf-8"))
+    rev = cln.recv(1024).decode("utf-8")
+    print(rev)
+    print(datetime.datetime.now())
+except Exception as e:
+  print(e)
+
+  
+  
+cln.close()
